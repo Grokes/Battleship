35226e4e5260ab04fdf99402203fa5bf310ea44a
milists <mihafuntt@gmail.com>
2023-09-27 21:11:35 +0300

    Добавьте файлы проекта.

---
+++ BattleShip.Core/Deck.cs
@@ -0,0 +1,15 @@
+﻿namespace BattleShip.Core
+{
+    internal class Deck
+    {
+        int Coord_x { get;}
+        int Coord_y { get;}
+        bool Is_dead { get; set; } = false;
+
+        public Deck(int x, int y)
+        { 
+            Coord_x = x;
+            Coord_y = y;
+        }
+    }
+}
---
+++ BattleShip.Core/GameField.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShip.Core
+{
+    enum Cell {Empty = 0, Ship = 1 }
+    internal class GameField
+    {
+        uint Size_x { get; set; }
+        uint Size_y { get; set; }
+        int[,] _field;
+        List<IShip> Ships { get; set; }
+
+        void CreateField()
+        { 
+            
+        }
+
+        void SetShip(IShip ship)
+        {
+            
+        }
+        bool Is_fieldEmpty(int x, int y)
+        {
+            return _field[x, y] == 0;
+        }
+    }
+}
